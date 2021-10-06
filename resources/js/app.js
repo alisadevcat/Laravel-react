d7433a64f7c4dd295edd6d71d5e3f0ddf1a55833
Alisa Raevskaya <alisa@madebyforward.com>
2021-10-06 23:19:04 +0300

    First commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+    import ReactDom from 'react-dom'
+    function Practice(){
+        return <h1>Hello World I am from React Functional Component</h1>
+    }
+    export default Practice
+    ReactDom.render(<Practice/>,document.getElementById('root'))
